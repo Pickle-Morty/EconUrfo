b509c575c5b92e1f1193cd5dfa7b8c430cdcf6ef
Pickle-Morty <avatarbliz@gmail.com>
2021-12-11 20:04:49 +0600

    first update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 document.querySelector('#btn-filter').onclick = () => {
-    console.log('ПУФ')
     document.querySelector('.filter').classList.toggle('active')
     document.querySelector('body').classList.toggle('locked')
     console.log(document.querySelector('.header__list').className, document.querySelector('.header__burger').className)
@@ -11,7 +10,6 @@ document.querySelector('#filter-close').onclick = () => {
 }
 
 document.querySelector('#header__burger').onclick = () => {
-    console.log("sdasd")
     document.querySelector('.menu-mobile').classList.add('active')
     document.querySelector('body').classList.add('locked')
 }
@@ -34,7 +32,6 @@ let collapseBtns = document.getElementsByClassName("collapsible");
 let collapseBlock = document.getElementsByClassName("collapse__wrap");
 
 for (let i = 0; i < collapseBtns.length; i++) {
-    console.log()
     if (collapseBlock[i].className == "collapse__wrap active") {
         collapseBlock[i].style.maxHeight = collapseBlock[i].scrollHeight + "px"
     }
@@ -44,7 +41,6 @@ for (let i = 0; i < collapseBtns.length; i++) {
     collapseBtns[i].addEventListener("click", function () {
         collapseBlock[i].classList.toggle("active")
         if (collapseBlock[i].style.maxHeight !== "0px") {
-            console.log("close")
             collapseBlock[i].style.maxHeight = 0
         }
         else {
